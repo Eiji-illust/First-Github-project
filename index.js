8c747035cf572a3edd3708e31f334a0d166de678
Eiji <gabcolendres@gmail.com>
2026-01-19 22:57:47 +0800

    Added a window prompt that asks for username

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 console.log("My first Github project");
 console.log("Learning Pull Requests🚀");
+console.log("Welcome!");
+
+let userName = prompt("What's your name?")
+                .split(",")
+                .trim();
+;
+!alert(`Hi ${userName}`);
