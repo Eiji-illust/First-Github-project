f3c52598c7e095c43640c424cb97d97283da6040
Eiji <gabcolendres@gmail.com>
2026-01-30 22:15:25 +0800

    LGTM

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Just me, learning Git and its confusing terminal :)
+Hi, There! I'm Eiji
+
+This is will be a documentation of my day-to-day progress as I learn javascript, it's syntax, and how to build things using this. I'll be updating this whenever I like it or when I have free time.
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ console.log("Welcome!");
 console.log("This is my first Github project");
 console.log("Day 1: Learning Pull Requests🚀");
 console.log("Day 2: Jumping straight to Javascript");
+console.log("Day 10+: I finally got a grasp of using functions, .includes()")
 
 let userName = prompt("What's your name?")
                 .trim();
