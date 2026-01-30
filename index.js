5ada8d613e7bab653e552de00630f7c1b2eb881a
Eiji <gabcolendres@gmail.com>
2026-01-30 22:19:04 +0800

    LGTM

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ console.log("Day 1: Learning Pull Requests🚀");
 console.log("Day 2: Jumping straight to Javascript");
 console.log("Day 10+: I finally got a grasp of using functions, .includes()")
 
+
+//  1/26 - I often read that I should try to learn DSA even if I'm 
+//          aiming to become a Front End Engineer so I did. Leetcode it is
+//  1/28 -  I barely understand BigO notations but I'm taking notes 
+//  1/30 - Today I learned about 
+
+
 let userName = prompt("What's your name?")
                 .trim();
 ;
